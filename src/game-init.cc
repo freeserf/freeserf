d09fad978f9a41ac2c53b1544084c188f92c5f71
Jon Lund Steffensen <jonlst@gmail.com>
2016-07-27 15:53:01 -0400

    game-init: Use correct preview generator for map type
    Fixes a bug where the random map preview did not correspond to
    the generated map. Previously the preview was generated with
    preserve_bugs enabled but the game map was generated with this
    setting disabled (in load_random_map()).
    
    Also fixes #219 by always generating mission previews with map
    size 3.

--- src/game-init.cc
+++ src/game-init.cc
@@ -447,12 +447,22 @@ game_init_box_t::generate_map_preview() {
   }
 
   map = new map_t();
-  map->init(map_size);
-  {
-    ClassicMapGenerator generator(map, mission->rnd);
-    generator.init(0, true);
-    generator.generate();
-    map->init_tiles(generator);
+  if (game_mission < 0) {
+    map->init(map_size);
+    {
+      ClassicMapGenerator generator(map, mission->rnd);
+      generator.init(0, true);
+      generator.generate();
+      map->init_tiles(generator);
+    }
+  } else {
+    map->init(3);
+    {
+      ClassicMissionMapGenerator generator(map, mission->rnd);
+      generator.init();
+      generator.generate();
+      map->init_tiles(generator);
+    }
   }
 
   minimap->set_map(map);
