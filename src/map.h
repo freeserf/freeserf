freeserf/freeserf

commit 5862cb3238095e1d480ad391fffcf248432787e1

Separate map tile members

src/map.h
@@ -290,13 +290,20 @@ class Map {
 
  protected:
   typedef struct Tile {
+    // Landscape filds
+    unsigned int height;
+    Terrain type_up;
+    Terrain type_down;
+    Minerals mineral;
+    int resource_amount;
+    // Mingled fields
+    Object obj;
+    // Game fields
     uint8_t paths;
-    uint8_t height;
-    uint8_t type;
-    uint8_t obj;
-    uint16_t obj_index;
-    uint8_t resource;
-    uint16_t serf;
+    unsigned int serf;
+    unsigned int owner;
+    bool idle_serf;
+    unsigned int obj_index;
   } Tile;
 
   /* Fundamentals */
@@ -384,37 +391,30 @@ class Map {
   void add_path(MapPos pos, Direction dir) { tiles[pos].paths |= BIT(dir); }
   void del_path(MapPos pos, Direction dir) { tiles[pos].paths &= ~BIT(dir); }
 
-  bool has_owner(MapPos pos) const { return ((tiles[pos].height >> 7) & 1); }
-  unsigned int get_owner(MapPos pos) const {
-                                        return ((tiles[pos].height >> 5) & 3); }
-  void set_owner(MapPos pos, unsigned int player) {
-    tiles[pos].height = (1 << 7) | (player << 5) | get_height(pos); }
-  void del_owner(MapPos pos) { tiles[pos].height &= 0x1f; }
-  unsigned int get_height(MapPos pos) const {
-    return (tiles[pos].height & 0x1f); }
+  bool has_owner(MapPos pos) const { return (tiles[pos].owner != 0); }
+  unsigned int get_owner(MapPos pos) const { return tiles[pos].owner; }
+  void set_owner(MapPos pos, unsigned int _owner) { tiles[pos].owner = _owner; }
+  void del_owner(MapPos pos) { tiles[pos].owner = 0; }
+  unsigned int get_height(MapPos pos) const { return tiles[pos].height; }
 
-  Terrain type_up(MapPos pos) const {
-    return static_cast<Terrain>(((tiles[pos].type >> 4) & 0xf)); }
-  Terrain type_down(MapPos pos) const {
-    return static_cast<Terrain>(tiles[pos].type & 0xf); }
+  Terrain type_up(MapPos pos) const { return tiles[pos].type_up; }
+  Terrain type_down(MapPos pos) const { return tiles[pos].type_down; }
   bool types_within(MapPos pos, Terrain low, Terrain high);
 
-  Object get_obj(MapPos pos) const {
-    return (Object)(tiles[pos].obj & 0x7f); }
-  unsigned int get_idle_serf(MapPos pos) const {
-    return ((tiles[pos].obj >> 7) & 1); }
-  void set_idle_serf(MapPos pos) { tiles[pos].obj |= BIT(7); }
-  void clear_idle_serf(MapPos pos) { tiles[pos].obj &= ~BIT(7); }
+  Object get_obj(MapPos pos) const { return tiles[pos].obj; }
+  bool get_idle_serf(MapPos pos) const { return tiles[pos].idle_serf; }
+  void set_idle_serf(MapPos pos) { tiles[pos].idle_serf = true; }
+  void clear_idle_serf(MapPos pos) { tiles[pos].idle_serf = false; }
 
   unsigned int get_obj_index(MapPos pos) const {
     return tiles[pos].obj_index; }
   void set_obj_index(MapPos pos, unsigned int index) {
     tiles[pos].obj_index = index; }
-  Minerals get_res_type(MapPos pos) const {
-    return (Minerals)((tiles[pos].resource >> 5) & 7); }
+  Minerals get_res_type(MapPos pos) const { return tiles[pos].mineral; }
   unsigned int get_res_amount(MapPos pos) const {
-    return (tiles[pos].resource & 0x1f); }
-  unsigned int get_res_fish(MapPos pos) const { return tiles[pos].resource; }
+    return tiles[pos].resource_amount; }
+  unsigned int get_res_fish(MapPos pos) const {
+    return tiles[pos].resource_amount; }
   unsigned int get_serf_index(MapPos pos) const { return tiles[pos].serf; }
 
   bool has_flag(MapPos pos) const { return (get_obj(pos) == ObjectFlag); }

src/map.cc
@@ -451,16 +451,13 @@ Map::init_tiles(const MapGenerator &generator) {
   for (unsigned int y = 0; y < rows; y++) {
     for (unsigned int x = 0; x < cols; x++) {
       MapPos pos_ = pos(x, y);
-      tiles[pos_].height = generator.get_height(pos_) & 0x1f;
-      tiles[pos_].type = (generator.get_type_up(pos_) & 0xf) << 4 |
-        (generator.get_type_down(pos_) & 0xf);
-      tiles[pos_].obj = generator.get_obj(pos_) & 0x7f;
-      if (generator.get_resource_type(pos_) != MineralsNone) {
-        tiles[pos_].resource = (generator.get_resource_type(pos_) & 7) << 5 |
-          (generator.get_resource_amount(pos_) & 0x1f);
-      } else {
-        tiles[pos_].resource = generator.get_resource_amount(pos_);
-      }
+      tiles[pos_].height = generator.get_height(pos_);
+      tiles[pos_].owner = 0;
+      tiles[pos_].type_up = generator.get_type_up(pos_);
+      tiles[pos_].type_down = generator.get_type_down(pos_);
+      tiles[pos_].obj = generator.get_obj(pos_);
+      tiles[pos_].mineral = generator.get_resource_type(pos_);
+      tiles[pos_].resource_amount = generator.get_resource_amount(pos_);
     }
   }
 
@@ -470,7 +467,7 @@ Map::init_tiles(const MapGenerator &generator) {
 /* Change the height of a map position. */
 void
 Map::set_height(MapPos pos, int height) {
-  tiles[pos].height = (tiles[pos].height & 0xe0) | (height & 0x1f);
+  tiles[pos].height = height;
 
   /* Mark landscape dirty */
   for (int d = DirectionRight; d <= DirectionUp; d++) {
@@ -486,7 +483,7 @@ Map::set_height(MapPos pos, int height) {
    building is removed. */
 void
 Map::set_object(MapPos pos, Object obj, int index) {
-  tiles[pos].obj = (tiles[pos].obj & 0x80) | (obj & 0x7f);
+  tiles[pos].obj = obj;
   if (index >= 0) tiles[pos].obj_index = index;
 
   /* Notify about object change */
@@ -501,18 +498,18 @@ Map::set_object(MapPos pos, Object obj, int index) {
 /* Remove resources from the ground at a map position. */
 void
 Map::remove_ground_deposit(MapPos pos, int amount) {
-  tiles[pos].resource -= amount;
+  tiles[pos].resource_amount -= amount;
 
-  if (get_res_amount(pos) == 0) {
+  if (tiles[pos].resource_amount <= 0) {
     /* Also sets the ground deposit type to none. */
-    tiles[pos].resource = 0;
+    tiles[pos].mineral = MineralsNone;
   }
 }
 
 /* Remove fish at a map position (must be water). */
 void
 Map::remove_fish(MapPos pos, int amount) {
-  tiles[pos].resource -= amount;
+  tiles[pos].resource_amount -= amount;
 }
 
 /* Set the index of the serf occupying map position. */
@@ -589,29 +586,28 @@ Map::update_public(MapPos pos, Random *rnd) {
 void
 Map::update_hidden(MapPos pos, Random *rnd) {
   /* Update fish resources in water */
-  if (is_in_water(pos) &&
-      tiles[pos].resource > 0) {
+  if (is_in_water(pos) && tiles[pos].resource_amount > 0) {
     int r = rnd->random();
 
-    if (tiles[pos].resource < 10 && (r & 0x3f00)) {
+    if (tiles[pos].resource_amount < 10 && (r & 0x3f00)) {
       /* Spawn more fish. */
-      tiles[pos].resource += 1;
+      tiles[pos].resource_amount += 1;
     }
 
     /* Move in a random direction of: right, down right, left, up left */
     MapPos adj_pos = pos;
     switch ((r >> 2) & 3) {
-    case 0: adj_pos = move_right(adj_pos); break;
-    case 1: adj_pos = move_down_right(adj_pos); break;
-    case 2: adj_pos = move_left(adj_pos); break;
-    case 3: adj_pos = move_up_left(adj_pos); break;
-    default: NOT_REACHED(); break;
+      case 0: adj_pos = move_right(adj_pos); break;
+      case 1: adj_pos = move_down_right(adj_pos); break;
+      case 2: adj_pos = move_left(adj_pos); break;
+      case 3: adj_pos = move_up_left(adj_pos); break;
+      default: NOT_REACHED(); break;
     }
 
     if (is_in_water(adj_pos)) {
       /* Migrate a fish to adjacent water space. */
-      tiles[pos].resource -= 1;
-      tiles[adj_pos].resource += 1;
+      tiles[pos].resource_amount -= 1;
+      tiles[adj_pos].resource_amount += 1;
     }
   }
 }
@@ -864,22 +860,27 @@ operator >> (SaveReaderBinary &reader, Map &map) {
       reader >> v8;
       map.tiles[pos].paths = v8 & 0x3f;
       reader >> v8;
-      map.tiles[pos].height = v8;
+      map.tiles[pos].height = v8 & 0x1f;
+      map.tiles[pos].owner = (v8 >> 5) & 0x07;
       reader >> v8;
-      map.tiles[pos].type = v8;
+      map.tiles[pos].type_up = (Map::Terrain)((v8 >> 4) & 0x0f);
+      map.tiles[pos].type_down = (Map::Terrain)(v8 & 0x0f);
       reader >> v8;
-      map.tiles[pos].obj = v8 & 0x7f;
+      map.tiles[pos].obj = (Map::Object)(v8 & 0x7f);
+      map.tiles[pos].idle_serf = BIT_TEST(v8, 7);
     }
     for (unsigned int x = 0; x < map.cols; x++) {
       MapPos pos = map.pos(x, y);
       if (map.get_obj(pos) >= Map::ObjectFlag &&
           map.get_obj(pos) <= Map::ObjectCastle) {
-        map.tiles[pos].resource = 0;
+        map.tiles[pos].mineral = Map::MineralsNone;
+        map.tiles[pos].resource_amount = 0;
         reader >> v16;
         map.tiles[pos].obj_index = v16;
       } else {
         reader >> v8;
-        map.tiles[pos].resource = v8;
+        map.tiles[pos].mineral = (Map::Minerals)((v8 >> 5) & 7);
+        map.tiles[pos].resource_amount = v8 & 0x1f;
         reader >> v8;
         map.tiles[pos].obj_index = 0;
       }
@@ -910,26 +911,41 @@ operator >> (SaveReaderText &reader, Map &map) {
       reader.value("paths")[y*SAVE_MAP_TILE_SIZE+x] >> val;
       map.tiles[p].paths = val & 0x3f;
 
-      reader.value("height")[y*SAVE_MAP_TILE_SIZE+x] >> val;
-      map.tiles[p].height = val & 0x1f;
+      try {
+        reader.value("owner")[y*SAVE_MAP_TILE_SIZE+x] >> val;
+        map.tiles[p].height = val;
+        reader.value("height")[y*SAVE_MAP_TILE_SIZE+x] >> map.tiles[p].owner;
+      } catch (...) {
+        reader.value("height")[y*SAVE_MAP_TILE_SIZE+x] >> val;
+        map.tiles[p].height = val & 0x1f;
+        map.tiles[p].owner = (val >> 5) & 0x07;
+      }
 
       reader.value("type.up")[y*SAVE_MAP_TILE_SIZE+x] >> val;
-      map.tiles[p].type = ((val & 0xf) << 4) | (map.tiles[p].type & 0xf);
+      map.tiles[p].type_up = (Map::Terrain)val;
 
       reader.value("type.down")[y*SAVE_MAP_TILE_SIZE+x] >> val;
-      map.tiles[p].type = (map.tiles[p].type & 0xf0) | (val & 0xf);
+      map.tiles[p].type_down = (Map::Terrain)val;
 
-      reader.value("object")[y*SAVE_MAP_TILE_SIZE+x] >> val;
-      map.tiles[p].obj = val & 0x7f;
+      try {
+        reader.value("idle_serf")[y*SAVE_MAP_TILE_SIZE+x] >> val;
+        map.tiles[p].idle_serf = val;
+        reader.value("object")[y*SAVE_MAP_TILE_SIZE+x] >> val;
+        map.tiles[p].obj = (Map::Object)val;
+      } catch (...) {
+        reader.value("object")[y*SAVE_MAP_TILE_SIZE+x] >> val;
+        map.tiles[p].obj = (Map::Object)(val & 0x7f);
+        map.tiles[p].idle_serf = BIT_TEST(val, 7);
+      }
 
       reader.value("serf")[y*SAVE_MAP_TILE_SIZE+x] >> val;
       map.tiles[p].serf = val;
 
       reader.value("resource.type")[y*SAVE_MAP_TILE_SIZE+x] >> val;
-      map.tiles[p].resource = ((val & 7) << 5) | (map.tiles[p].resource & 0x1f);
+      map.tiles[p].mineral = (Map::Minerals)val;
 
       reader.value("resource.amount")[y*SAVE_MAP_TILE_SIZE+x] >> val;
-      map.tiles[p].resource = (map.tiles[p].resource & 0xe0) | (val & 0x1f);
+      map.tiles[p].resource_amount = val;
     }
   }
 
@@ -952,11 +968,13 @@ operator << (SaveWriterText &writer, Map &map) {
           MapPos pos = map.pos(tx+x, ty+y);
 
           map_writer.value("height") << map.get_height(pos);
+          map_writer.value("owner") << map.get_owner(pos);
           map_writer.value("type.up") << map.type_up(pos);
           map_writer.value("type.down") << map.type_down(pos);
           map_writer.value("paths") << map.paths(pos);
           map_writer.value("object") << map.get_obj(pos);
           map_writer.value("serf") << map.get_serf_index(pos);
+          map_writer.value("idle_serf") << map.get_idle_serf(pos);
 
           if (map.is_in_water(pos)) {
             map_writer.value("resource.type") << 0;

commit 7dc1da444394a72c58810f022911e9691b1a70db

Fix regression causing incorrect land owner
Fix regression introduced in 5862cb3 where owner == 0 was changed to
mean that the land has no owner. This resulted in the first player
(player 0) not being able to own any land. Fixed by incrementing
the stored player number by one such that 0 indicates no owner,
player 0 is indicated by 1, and so on.

src/map.h
@@ -398,9 +398,10 @@ class Map {
     game_tiles[pos].paths &= ~BIT(dir); }
 
   bool has_owner(MapPos pos) const { return (game_tiles[pos].owner != 0); }
-  unsigned int get_owner(MapPos pos) const { return game_tiles[pos].owner; }
+  unsigned int get_owner(MapPos pos) const {
+    return game_tiles[pos].owner - 1; }
   void set_owner(MapPos pos, unsigned int _owner) {
-    game_tiles[pos].owner = _owner; }
+    game_tiles[pos].owner = _owner + 1; }
   void del_owner(MapPos pos) { game_tiles[pos].owner = 0; }
   unsigned int get_height(MapPos pos) const {
     return landscape_tiles[pos].height; }