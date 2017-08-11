00e14aeaa0706997616280843792cc2e52883b6e
wsigger <wicked_digger@mail.ru>
2017-08-11 12:20:52 +0300

    Switch testing to GTest

--- tests/test_map.cc
+++ tests/test_map.cc
@@ -1,7 +1,7 @@
 /*
- * test_map.cc - TAP test for classic mission map generator
+ * test_map.cc - test for classic mission map generator
  *
- * Copyright (C) 2016  Jon Lund Steffensen <jonlst@gmail.com>
+ * Copyright (C) 2016-2017  Jon Lund Steffensen <jonlst@gmail.com>
  *
  * This file is part of freeserf.
  *
@@ -19,6 +19,8 @@
  * along with freeserf.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <gtest/gtest.h>
+
 #include <cstdlib>
 #include <iostream>
 #include <fstream>
@@ -31,11 +33,7 @@
 #include "src/random.h"
 
 
-int
-main(int argc, char *argv[]) {
-  /* Print number of tests for TAP */
-  std::cout << "1..1" << "\n";
-
+TEST(Map, ClassicMissionMapGenerator) {
   int map_size = 3;
 
   // Read map data from memory dump
@@ -45,10 +43,7 @@ main(int argc, char *argv[]) {
   std::string path = src_dir;
   path += "/tests/data/map-memdump-1";
   std::ifstream is(path.c_str(), std::ifstream::binary);
-  if (!is.good()) {
-    std::cerr << "Error opening file!\n";
-    return 1;
-  }
+  ASSERT_TRUE(is.good()) << "Error opening file! " << path;
 
   std::istreambuf_iterator<char> is_start(is), is_end;
   std::vector<char> data(is_start, is_end);
@@ -67,10 +62,7 @@ main(int argc, char *argv[]) {
 
   // Check that data is matching
   int expected_size = map.get_rows() * map.get_cols() * 8;
-  if (data.size() != expected_size) {
-    std::cerr << "Unexpected data size: " << data.size() << "\n";
-    return 1;
-  }
+  EXPECT_EQ(expected_size, data.size()) << "Unexpected data size";
 
   // Find offset for map dump indices
   int index_offset = 0;
@@ -78,81 +70,60 @@ main(int argc, char *argv[]) {
     index_offset += 1;
   }
 
-  int errors = 0;
-  for (unsigned int y = 0; y < map.get_rows(); y++) {
-    for (unsigned int x = 0; x < map.get_cols(); x++) {
+  for (int y = 0; y < map.get_rows(); y++) {
+    for (int x = 0; x < map.get_cols(); x++) {
       MapPos pos = map.pos(x, y);
       int index_1 = ((y << index_offset) | x) << 2;
       int index_2 = ((y << index_offset) | (1 << (index_offset-1)) | x) << 2;
 
       // Height
       int height = data[index_1+1] & 0x1f;
-      if (height != generator.get_height(pos)) {
-        std::cerr << "Invalid height at " << x << "," << y << " is " <<
-          generator.get_height(pos) << " should have been " << height << "\n";
-        errors += 1;
-      }
+      EXPECT_EQ(height, generator.get_height(pos)) <<
+        "Invalid height at " << x << "," << y << " is " <<
+          generator.get_height(pos) << " should have been " << height;
 
       // Type up
       int type_up = (data[index_1+2] & 0xf0) >> 4;
-      if (type_up != generator.get_type_up(pos)) {
-        std::cerr << "Invalid type up at " << x << "," << y << " is " <<
+      EXPECT_EQ(type_up, generator.get_type_up(pos)) <<
+        "Invalid type up at " << x << "," << y << " is " <<
           generator.get_type_up(pos) << " should have been " <<
-          type_up << "\n";
-        errors += 1;
-      }
+          type_up;
 
       // Type down
       int type_down = data[index_1+2] & 0xf;
-      if (type_down != generator.get_type_down(pos)) {
-        std::cerr << "Invalid type down at " << x << "," << y << " is " <<
+      EXPECT_EQ(type_down, generator.get_type_down(pos)) <<
+        "Invalid type down at " << x << "," << y << " is " <<
           generator.get_type_down(pos) << " should have been " <<
-          type_down << "\n";
-        errors += 1;
-      }
+          type_down;
 
       // Object
       int object = data[index_1+3] & 0x7f;
-      if (object != generator.get_obj(pos)) {
-        std::cerr << "Invalid object at " << x << "," << y << " is " <<
-          generator.get_obj(pos) << " should have been " << object << "\n";
-        errors += 1;
-      }
+      EXPECT_EQ(object, generator.get_obj(pos)) <<
+        "Invalid object at " << x << "," << y << " is " <<
+          generator.get_obj(pos) << " should have been " << object;
 
       if (type_up <= Map::TerrainWater3) {
         // Fish resource
         int fish_amount = data[index_2];
-        if (fish_amount != generator.get_resource_amount(pos)) {
-          std::cerr << "Invalid fish amount at " << x << "," << y <<
+        EXPECT_EQ(fish_amount, generator.get_resource_amount(pos)) <<
+          "Invalid fish amount at " << x << "," << y <<
             " is " << generator.get_resource_amount(pos) <<
-            " should have been " << fish_amount << "\n";
-          errors += 1;
-        }
+            " should have been " << fish_amount;
       } else {
         // Resource type
         int res_type = (data[index_2] & 0xe0) >> 5;
-        if (res_type != generator.get_resource_type(pos)) {
-          std::cerr << "Invalid resource type at " << x << "," << y <<
+        EXPECT_EQ(res_type, generator.get_resource_type(pos)) <<
+          "Invalid resource type at " << x << "," << y <<
             " is " << generator.get_resource_type(pos) <<
-            " should have been " << res_type << "\n";
-          errors += 1;
-        }
+            " should have been " << res_type;
 
         // Resource amount
         int res_amount = data[index_2] & 0x1f;
-        if (res_amount != generator.get_resource_amount(pos)) {
-          std::cerr << "Invalid resource amount at " << x << "," << y <<
+        EXPECT_EQ(res_amount, generator.get_resource_amount(pos)) <<
+          "Invalid resource amount at " << x << "," << y <<
             " is " << generator.get_resource_amount(pos) <<
-            " should have been " << res_amount << "\n";
-          errors += 1;
-        }
+            " should have been " << res_amount;
       }
     }
-  }
-
-  if (errors > 0) {
-    std::cout << "not ok 1 - Found " << errors << " map errors!\n";
-  } else {
-    std::cout << "ok 1 - Map is identical to memory dump.\n";
   }
 }
--- tests/test_save_game.cc
+++ tests/test_save_game.cc
@@ -1,5 +1,5 @@
 /*
- * test_save_game.cc - TAP test for loading/saving game
+ * test_save_game.cc - test for loading/saving game
  *
  * Copyright (C) 2016-2017  Jon Lund Steffensen <jonlst@gmail.com>
  *
@@ -19,6 +19,8 @@
  * along with freeserf.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <gtest/gtest.h>
+
 #include <iostream>
 #include <sstream>
 #include <memory>
@@ -28,11 +30,8 @@
 #include "src/savegame.h"
 #include "src/mission.h"
 
-int
-main(int argc, char *argv[]) {
-  // Print number of tests for TAP
-  std::cout << "1..9" << "\n";
 
+TEST(SaveGame, RandomMapSaveGame) {
   // Create random map game
   std::unique_ptr<Game> game(new Game());
   game->init(3, Random("8667715887436237"));
@@ -40,82 +39,41 @@ main(int argc, char *argv[]) {
   game->add_player(35, 30, 40);
 
   Player *player_0 = game->get_player(0);
-  if (player_0 == NULL) {
-    std::cout << "not ok 1 - Player not added to game\n";
-    return 0;
-  } else {
-    std::cout << "ok 1 - Player added to game\n";
-  }
+  ASSERT_TRUE(player_0 != NULL);
 
   // Build castle
-  if (game->build_castle(game->get_map()->pos(6, 6), player_0)) {
-    std::cout << "ok 2 - Player built castle\n";
-  } else {
-    std::cout << "not ok 2 - Player was not able to build castle\n";
-    return 0;
-  }
+  bool r = game->build_castle(game->get_map()->pos(6, 6), player_0);
+  ASSERT_TRUE(r) << "Player was not able to build castle";
 
   // Run game for a number of ticks
   for (int i = 0; i < 500; i++) game->update();
-
-  std::cout << "ok 3 - Random map game started and running\n";
 
   // Save the game state
   std::stringstream str;
   bool saved = GameStore::get_instance()->write(&str, game.get());
   str.flush();
 
-  if (saved && str.good()) {
-    std::cout << "ok 4 - Saved game state\n";
-  } else {
-    std::cout << "not ok 4 - Failed to save game state; returned "
-              << saved << "\n";
-    return 0;
-  }
+  ASSERT_TRUE(saved && str.good()) <<
+    "Failed to save game state; returned " << saved;
 
   // Load the game state into a new game
   str.seekg(0, std::ios::beg);
   std::unique_ptr<Game> loaded_game(new Game());
   bool loaded = GameStore::get_instance()->read(&str, loaded_game.get());
 
-  if (loaded) {
-    std::cout << "ok 5 - Loaded game state\n";
-  } else {
-    std::cout << "not ok 5 - Failed to load save game state; returned "
-              << loaded << "\n";
-    return 0;
-  }
+  ASSERT_TRUE(loaded) <<
+    "Failed to load save game state; returned " << loaded;
 
   // Check map
-  if (*game->get_map() == *loaded_game->get_map()) {
-    std::cout << "ok 6 - Maps are equal\n";
-  } else {
-    std::cout << "not ok 6 - Map equality test failed\n";
-  }
+  EXPECT_EQ(*game->get_map(), *loaded_game->get_map());
 
   // Check gold deposit
-  if (game->get_gold_total() == loaded_game->get_gold_total()) {
-    std::cout << "ok 7 - Total gold count is identical\n";
-  } else {
-    std::cout << "not ok 7 - Total gold count is not identical\n";
-  }
+  EXPECT_EQ(game->get_gold_total(), loaded_game->get_gold_total());
 
   // Check player
   Player *loaded_player_0 = loaded_game->get_player(0);
-  if (loaded_player_0 != NULL) {
-    std::cout << "ok 8 - Player exists in loaded game\n";
-  } else {
-    std::cout << "not ok 8 - Player does not exist in loaded game\n";
-    return 0;
-  }
+  ASSERT_TRUE(loaded_player_0 != NULL);
 
   // Check player land area
-  if (player_0->get_land_area() == loaded_player_0->get_land_area()) {
-    std::cout << "ok 9 - Player land area is identical\n";
-  } else {
-    std::cout << "not ok 9 - Player land area is not identical "
-                 "(in saved game - " << player_0->get_land_area()
-              << " but in loaded game - " << loaded_player_0->get_land_area()
-              << ")\n";
-  }
+  EXPECT_EQ(player_0->get_land_area(), loaded_player_0->get_land_area());
 }
