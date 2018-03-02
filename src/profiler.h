e4b3d8f89922d2ea270702bdcd491e22b9f6ae15
Yury Klushin <wicked_digger@mail.ru>
2018-03-02 22:21:36 +0300

    Add: profiler tool

---
+++ src/profiler.h
@@ -0,0 +1,30 @@
+/*
+ * profiler.h - Profiling tool.
+ *
+ * Copyright (C) 2018   Wicked_Digger <wicked_digger@mail.ru>
+ *
+ * This file is part of freeserf.
+ *
+ * freeserf is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * freeserf is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with freeserf.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef SRC_PROFILER_H_
+#define SRC_PROFILER_H_
+
+/* The length between game updates in miliseconds. */
+#define TICK_LENGTH  20
+#define TICKS_PER_SEC  (1000/TICK_LENGTH)
+
+
+#endif  // SRC_PROFILER_H_
