f9b03cae125955475dd0df58ce7cc65c39d362c6
Mariusz Bialonczyk <manio@skyboo.net>
2024-01-30 17:41:19 +0100

    rename_macros.py

--- src/savegame.cc
+++ src/savegame.cc
@@ -41,7 +41,7 @@
 #include <Knownfolders.h>
 #include <Shlobj.h>
 #include <direct.h>
-#elif defined(__APPLE__)
+#elif defined(SDL_PLATFORM_APPLE)
 #include <dirent.h>
 #include <sys/stat.h>
 #else
@@ -485,7 +485,7 @@ GameStore::GameStore() {
                         (LPSTR)folder_path.data(), len, nullptr, nullptr);
   ::CoTaskMemFree(saved_games_path);
   folder_path += '\\';
-#elif defined(__APPLE__)
+#elif defined(SDL_PLATFORM_APPLE)
   folder_path = std::string(std::getenv("HOME"));
   folder_path += "/Library/Application Support";
   folder_path += '/';
